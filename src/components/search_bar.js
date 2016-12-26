824e8ca361f3d2fec0c5cc0114c0223bdc9ff29b
Paul WB <higgsbison@gmail.com>
2016-12-25 17:41:12 -0800

    add styling. implement search

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -5,16 +5,21 @@ class SearchBar extends Component {
     super(props);
     this.state = {term: ''};
   }
+  onInputChange(term) {
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
   render() {
     return (
-      <div>
+      <div className='search-bar'>
         <input
           value={this.state.term}
-          onChange={(e) => this.setState({term: e.target.value})}
+          onChange={(e) => this.onInputChange(e.target.value)}
         />
       </div>
     );
   }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -14,15 +14,17 @@ class App extends Component {
       videos: [],
       selectedVideo: null,
     };
-
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term}, (videos) => {
       this.setState({videos, selectedVideo: videos[0]});
     });
   }
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={(term) => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={(selectedVideo) => this.setState({selectedVideo})}
